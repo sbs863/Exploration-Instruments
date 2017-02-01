ecec6a63487f85a76384f717b0abf4e6d4ea5de8
sbs863 <samtrey@gmail.com>
2017-02-01 15:44:42 -0600

    hopefully secure now

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\forceSchema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        if (App::environment('remote')) {
+            URL::forceSchema('https');
+        };
     }
 
     /**
