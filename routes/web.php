2d451cbdb12c72b125085c23bc16d9a4f6f8b23d
sbs863 <samtrey@gmail.com>
2017-02-01 15:47:14 -0600

    hopefully secure now

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-if (App::environment('remote')) {
+if (App::environment('production')) {
     URL::forceSchema('https');
 };
 
