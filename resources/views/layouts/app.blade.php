84a3a163c404c10e158698841be5c99941f0ffa4
sbs863 <samtrey@gmail.com>
2017-02-01 15:05:56 -0600

    css link typo corrected

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,11 @@
     <title>@yield('title')</title>
 
     <!-- Styles -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
     <link href="{{ secure_asset('css/style.css') }}" media="all" rel="stylesheet" type="text/css"/>
-    <link href="{{ secure_asset('css/style.css') }}" media="all" rel="stylesheet" type="text/css"/>
+
     <!-- Scripts -->
     <script>
         window.Laravel = <?php echo json_encode(['csrfToken' => csrf_token(),
@@ -24,104 +24,104 @@
 </head>
 <body>
 
-    {{--<div class="row-fluid top">--}}
-    {{--<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">--}}
-    {{--<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 logo ">--}}
-    {{--<img src="../../images/logoSmall.png" class="exi" alt="Image">--}}
-    {{--</div>--}}
-    {{--<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 header">--}}
-    {{--EXPLORATION INSTRUMENTS--}}
-    {{--</div>--}}
+{{--<div class="row-fluid top">--}}
+{{--<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">--}}
+{{--<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 logo ">--}}
+{{--<img src="../../images/logoSmall.png" class="exi" alt="Image">--}}
+{{--</div>--}}
+{{--<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 header">--}}
+{{--EXPLORATION INSTRUMENTS--}}
+{{--</div>--}}
 
-    {{--</div>--}}
-    {{--</div>--}}
+{{--</div>--}}
+{{--</div>--}}
 
-            <nav class="navbar navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-fixed-top" role="navigation">
 
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed " data-toggle="modal" data-target="#nav-modal"
-                            aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a class="navItem" href="/home">Home</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false"> Equipment
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed " data-toggle="modal" data-target="#nav-modal"
+                aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li class="active"><a class="navItem" href="/home">Home</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                   aria-expanded="false"> Equipment
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a class="navItem" href="/rentals">For Rent</a>
+                        <a class="navItem" href="/saless">For Sale</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li><a class="navItem" href="/about">About Us</a></li>
+            <li><a class="navItem" href="/news">News & Articles</a></li>
+            <li><a class="navItem" href="/newCustomer">New Customer Forms</a></li>
+            <li><a class="navItem" href="/portal">Customer Portal</a></li>
+            <li><a class="navItem" href="/contact">Contact</a></li>
+        </ul>
+        @if (Auth::guest())
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="{{ secure_url('/login') }}">Login</a></li>
+                <li><a href="{{ secure_url('/register') }}">Register</a></li>
+            </ul>
+        @else
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                       aria-expanded="false">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{ secure_url('/logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                Logout
                             </a>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a class="navItem" href="/rentals">For Rent</a>
-                                    <a class="navItem" href="/saless">For Sale</a>
-                                </li>
-                            </ul>
+                            <form id="logout-form" action="{{ secure_url('/logout') }}" method="POST"
+                                  style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
                         </li>
-
-                        <li><a class="navItem" href="/about">About Us</a></li>
-                        <li><a class="navItem" href="/news">News & Articles</a></li>
-                        <li><a class="navItem" href="/newCustomer">New Customer Forms</a></li>
-                        <li><a class="navItem" href="/portal">Customer Portal</a></li>
-                        <li><a class="navItem" href="/contact">Contact</a></li>
                     </ul>
-                    @if (Auth::guest())
-                        <ul class="nav navbar-nav navbar-right">
-                        <li><a href="{{ secure_url('/login') }}">Login</a></li>
-                        <li><a href="{{ secure_url('/register') }}">Register</a></li>
-                        </ul>
-                    @else
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                   aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                </li>
+            </ul>
+        @endif
+    </div>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ secure_url('/logout') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ secure_url('/logout') }}" method="POST"
-                                              style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    @endif
-                </div>
-
-            </nav>
-            <!-- Modal -->
-            <div class="modal fade" id="nav-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-nav-content">
-                        <div class="modal-nav-body">
-                            <ul>
-                                <li class="active"><a class="navItem" href="/home">Home</a></li>
-                                <li><a class="modalA" href="/rentals">For Rent</a></li>
-                                <li><a class="modalA" href="/sales">For Sale</a></li>
-                                <li><a class="modalA" href="/about">About Us</a></li>
-                                <li><a class="modalA" href="/news">News & Articles</a></li>
-                                <li><a class="modalA" href="/newCustomer">New Customer Forms</a></li>
-                                <li><a class="modalA" href="/portal">Customer Portal</a></li>
-                                <li><a class="modalA" href="/contact">Contact</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+</nav>
+<!-- Modal -->
+<div class="modal fade" id="nav-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-nav-content">
+            <div class="modal-nav-body">
+                <ul>
+                    <li class="active"><a class="navItem" href="/home">Home</a></li>
+                    <li><a class="modalA" href="/rentals">For Rent</a></li>
+                    <li><a class="modalA" href="/sales">For Sale</a></li>
+                    <li><a class="modalA" href="/about">About Us</a></li>
+                    <li><a class="modalA" href="/news">News & Articles</a></li>
+                    <li><a class="modalA" href="/newCustomer">New Customer Forms</a></li>
+                    <li><a class="modalA" href="/portal">Customer Portal</a></li>
+                    <li><a class="modalA" href="/contact">Contact</a></li>
+                </ul>
             </div>
-    <script type="text/javascript" src="{{ secure_asset('js/app.js') }}"></script>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="{{ secure_asset('js/app.js') }}"></script>
 @yield('body')
 
 
