bfdfe9749b1f281ed781e923004ee508e1477591
sbs863 <samtrey@gmail.com>
2017-02-01 11:29:48 -0600

    database character lengths updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name', 32);
+            $table->string('email',250)->unique();
+            $table->string('password',64);
             $table->rememberToken();
             $table->timestamps();
         });
