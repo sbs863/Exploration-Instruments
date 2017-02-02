da808dc819ad52f1cf12f0c340959c756b574764
sbs863 <samtrey@gmail.com>
2017-02-01 18:54:10 -0600

    product page css

--- resources/views/pages/rentals.blade.php
+++ resources/views/pages/rentals.blade.php
@@ -5,34 +5,34 @@
 
 @section('body')
     <link href="{{ secure_asset('css/products.css') }}" media="all" rel="stylesheet" type="text/css"/>
-  <header>
-  	<h1>EX</h1>
-  </header>
+    <header>
+        <h1>EX</h1>
+    </header>
 
 
     <div class="container">
-     <div class="row">
+        <div class="row galleryRow">
 
             <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-              <div id="product_list">
-              </div>
+                <div id="product_list">
+                </div>
             </div>
-            <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
-             <div class="panel panel-default">
-              <div class="panel-heading">
-               <h3 class="panel-title">Products Types</h3>
-              </div>
-              <div class="panel-body" style="max-height:600px;overflow-y: scroll;">
-               <div id="product_Type" class="row">
+            <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 gallery">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Products Types</h3>
+                    </div>
+                    <div class="panel-body" style="overflow-y: scroll;">
+                        <div id="product_Type" class="row">
 
-               </div>
-              </div>
-              </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
+        </div>
     </div>
-  </div>
 @endsection
 
 <script src="{{ secure_asset('js/app.js') }}"></script>
