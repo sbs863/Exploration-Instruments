7009417849058fb6ef39483a329a783fa5bb0707
sbs863 <samtrey@gmail.com>
2017-02-01 18:26:58 -0600

    product page css

--- resources/views/pages/rentals.blade.php
+++ resources/views/pages/rentals.blade.php
@@ -18,7 +18,7 @@
 
               </div>
             </div>
-            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-lg-offset-1">
+            <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 col-lg-o">
              <div class="panel panel-default">
               <div class="panel-heading">
                <h3 class="panel-title">Products Types</h3>
